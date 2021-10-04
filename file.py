12a225f043748327c6b5b69d2a6e77d826e00916
Kartheek Surampudi <39260345+k4rth33k@users.noreply.github.com>
2021-10-04 23:06:53 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,13 @@
+import time
+from PIL import Image
+
+with open('/tmp/tempfile.txt', 'w') as fp:
+    fp.write('This is from /tmp/tempfile.txt')
+
+with open('sample.log', 'w') as fp:
+    fp.write('This is from sample.log')
+
+# time.sleep(10)
+
+im = Image.new(mode="RGB", size=(200, 200))
+im.save('test.jpg')
