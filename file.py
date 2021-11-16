9b562c41749880894429588ef38cb979abdd0851
Kartheek Surampudi <39260345+k4rth33k@users.noreply.github.com>
2021-11-16 18:52:50 +0530

    Update file.py

--- file.py
+++ file.py
@@ -5,12 +5,14 @@ with open('/tmp/tempfile.txt', 'w') as fp:
     print('Writing to tempfile')
     fp.write('This is from /tmp/tempfile.txt')
 
+fp = open('sample', 'a')
 for _ in range(1000000):
-    with open('sample', 'a') as fp:
-        print('Writing to sample.log')
-        fp.write('This is from sample.log')
+    print('Writing to sample.log')
+    fp.write('This is from sample.log')
 
     time.sleep(0.5)
+
+fp.close()
 
 im = Image.new(mode="RGB", size=(200, 200))
 im.save('test.jpg')
