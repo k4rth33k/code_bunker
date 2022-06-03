ccdf7dc181f27f7b05f416d2c2d920482de977e5
k4rth33k <kartheek2000mike@gmail.com>
2022-06-03 12:41:50 +0530

    SHort test

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import tensorflow as tf
+import torch
+import time
+import argparse
+
+
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+
+parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+args = parser.parse_args()
+
+
+time.sleep(30)
+print(args.batch_size)
