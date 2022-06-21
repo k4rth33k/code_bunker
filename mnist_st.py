e0b9ea441866b50d6cd2308d5b61063bd035fb86
k4rth33k <kartheek2000mike@gmail.com>
2022-06-21 23:36:12 +0530

    Remove st

--- mnist_st.py
+++ mnist_st.py
@@ -13,12 +13,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
-from nni.utils import merge_parameter
 from torchvision import datasets, transforms
 import time
 
-import scaletorch as st
-st.init(verbose=None)
+# import scaletorch as st
+# st.init(verbose=True)
 
 logger = logging.getLogger('mnist_AutoML')
 
@@ -44,7 +43,8 @@ class Net(nn.Module):
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        if (args['batch_num'] is not None) and batch_idx >= args['batch_num']:
+        continue
+        if (args.batch_num is not None) and batch_idx >= args.batch_num:
             break
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -53,9 +53,9 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss.backward()
         optimizer.step()
 
-        st.track(epoch=epoch, metrics={'train_loss' : loss.item()}, tuner_default='train_loss')
+        # st.track(epoch=epoch, metrics={'train_loss' : loss.item()}, tuner_default='train_loss')
 
-        if batch_idx % args['log_interval'] == 0:
+        if batch_idx % args.log_interval == 0:
             logger.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
@@ -86,15 +86,15 @@ def test(args, model, device, test_loader):
 
 
 def main(args):
-    use_cuda = not args['no_cuda'] and torch.cuda.is_available()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
-    torch.manual_seed(args['seed'])
+    torch.manual_seed(args.seed)
 
     device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
-    data_dir = args['data_dir']
+    data_dir = args.data_dir
 
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(data_dir, train=True, download=True,
@@ -102,7 +102,7 @@ def main(args):
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
-        batch_size=args['batch_size'], shuffle=True, **kwargs)
+        batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST(data_dir, train=False, transform=transforms.Compose([
             transforms.ToTensor(),
@@ -110,30 +110,29 @@ def main(args):
         ])),
         batch_size=1000, shuffle=True, **kwargs)
 
-    hidden_size = args['hidden_size']
+    hidden_size = args.hidden_size
 
     model = Net(hidden_size=hidden_size).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args['lr'],
-                          momentum=args['momentum'])
+    optimizer = optim.SGD(model.parameters(), lr=args.lr,
+                          momentum=args.momentum)
 
-    writer = SummaryWriter(f'{st.get_artifacts_dir()}/tensorboard')
+    # writer = SummaryWriter(f'{st.get_artifacts_dir()}/tensorboard')
+    writer = SummaryWriter('tensorboard')
 
-    for epoch in range(1, args['epochs'] + 1):
+    for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test_acc = test(args, model, device, test_loader)
 
         # report intermediate result
         # nni.report_intermediate_result(test_acc)
         writer.add_scalar('test_acc', test_acc, epoch)
-        st.track(epoch, {'test_acc' : test_acc}, 'test_acc')
+        # st.track(epoch, {'test_acc' : test_acc}, 'test_acc')
         logger.debug('test accuracy %g', test_acc)
         logger.debug('Pipe send intermediate result done.')
 
         # if epoch % 3 == 0:
         logger.debug('Saving model')
-        st.torch.save(model.state_dict(), 'model.pth', metadata={'test_acc' : test_acc})
-        
-        time.sleep(70)
+        # st.torch.save(model.state_dict(), 'model.pth', metadata={'test_acc' : test_acc})
 
     # report final result
     # nni.report_final_result(test_acc)
@@ -172,10 +171,9 @@ def get_params():
 
 if __name__ == '__main__':
     try:
+        # print(torch.cuda.get_device_name(0))
         # get parameters form tuner
-        tuner_params = nni.get_next_parameter()
-        logger.debug(tuner_params)
-        params = vars(merge_parameter(get_params(), tuner_params))
+        params = get_params()
         print(params)
         main(params)
     except Exception as exception:
